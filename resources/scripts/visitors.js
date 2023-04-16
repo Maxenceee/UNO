c0168e0f8f7f00e2d029b8f5701776bda8bf67c5
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-04-16 22:52:39 +0200

    add dir creation is absent

--- resources/scripts/visitors.js
+++ resources/scripts/visitors.js
@@ -4,10 +4,15 @@ const dotenv = require('dotenv');
 let instance = null;
 dotenv.config();
 
+let dirlogsPath = require('path').resolve(__dirname, "../logs");
 let logsPath = require('path').resolve(__dirname, "../logs/requests.log");
 
 class Visitors {
     static getVisitorsInstance() {
+        // create logs dir if does exist
+        if (!fs.existsSync(logsPath)) {
+            fs.mkdirSync(dirlogsPath);
+        }
         return instance ? instance : new Visitors();
     }
 
